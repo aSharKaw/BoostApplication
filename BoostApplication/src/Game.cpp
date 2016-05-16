395550a30c67cee0e1323b805ae099a9c1dab3ce
xSYUxx <syu_neko06@yahoo.co.jp>
2016-05-16 18:04:46 +0900

    VSCodeからのtest

--- BoostApplication/src/Game.cpp
+++ BoostApplication/src/Game.cpp
@@ -12,11 +12,6 @@ Game::Game()
 	player_name = "";
 	player_level = player_maxhp = player_hp = player_maxmp = player_mp = player_exp = 0;
 	
-
-	//enemy_name = enemy.getEnemyName("enemy1.Name");
-	//enemy_hp = enemy.getEnemyStatus("enemy1.hp");
-	//enemy_mp = enemy.getEnemyStatus("enemy1.mp");
-	//enemy_exp = enemy.getEnemyStatus("enemy1.exp");
 }
 
 
@@ -29,6 +24,7 @@ void Game::Title()
 	string _mode;
 
 
+
 	cout << format("%1%%2%%3%") 
 		% "-------\n" 
 		% " Title\n" 
@@ -37,20 +33,24 @@ void Game::Title()
 		% "・New  Game\n" 
 		% "・Load Game\n";
 
+
 	cout << "Select Menu->(n/l)" << endl;
 	cin >> _mode;
 
 	if (_mode == "n")
 	{
 		std::system("cls");
+
+
 		cout << format("%1%%2%%3%")
 			% "Welcome World!!\n\n"
 			% "Create Player\n"
 			% "Player Name? ->";
 		cin >> player_name;
 
+
 		cout << format("%1%")
-			% "...OK! Enjoy World!!";
+			% "...OK!";
 
 		save.Saving("res/save1.ini", player_name, 1);
 	}
@@ -58,22 +58,42 @@ void Game::Title()
 	{
 		std::system("cls");
 
+		
 		player_name = load.getName();
 		player_level = load.getLevel();
+		
+		
+		cout << format("%1%%2%%3%%4%")
+			% "Loaded \nPlayerName : "
+			% player_name
+			% "\nPlayerLevel : "
+			% player_level << endl;
 	}
 	
 }
 
 void Game::Buttle()
 {
+	string command;
+
 	//敵のロード
 	enemy_name = enemy.getEnemyName("Enemy1.Name");
 	enemy_hp = enemy.getEnemyStatus("Enemy1.hp");
 	enemy_mp = enemy.getEnemyStatus("Enemy1.mp");
 	enemy_exp = enemy.getEnemyStatus("Enemy1.exp");
 
+
+
 	cout << format("%1%%2%%3%")
-		% "Encount "
+		% "\n\nEncount "
 		% enemy_name
 		% "!!\n";
+
+
+	cout << format("%1%")
+		% "Command? ->";
+	cin >> command;
+
+
+
 }
